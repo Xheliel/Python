8222a99c7a0264079595ad549d57670558aca90d
Eliel Santos <161032378+Xheliel@users.noreply.github.com>
2024-05-22 12:37:13 +0000

    sono

---
+++ Aulas/6º aula/Atividades/5. Atividade.py
@@ -0,0 +1,18 @@
+# não finalizado!
+import os
+
+
+def cabecalho():
+    os.system("clear")
+    print("---------------------")
+    print("=== SENAI | FIEB ===")
+    print("---------------------")
+
+print("\n")
+
+numero1 = float(input("Digite um número: "))
+numero2 = float(input("Digite outro número: "))
+
+def soma(numero1, numero2):
+    resultado = numero1 + numero2
+    return resultado
